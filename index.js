383c34208bdd686107da4cec139c6779cd056f7a
Rajat Mishra <85543339+hydrox19@users.noreply.github.com>
2022-01-31 09:56:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 exports.RestClient = require("./src/RestClient");
 exports.InteractionServer = require("./src/InteractionServer");
 exports.WebsocketShard = require("./src/WebsocketShard");
+exports.InternalSharder = require("./src/InternalSharder.js")
