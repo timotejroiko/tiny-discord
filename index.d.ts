9a9b413f2f0c63a8a9dc25d5d87fa3fdc1d5125e
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2022-04-15 11:45:26 -0300

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module "tiny-discord" {
 	import EventEmitter from "events"
 
-	export type DiscordEvent = "APPLICATION_COMMAND_CREATE | APPLICATION_COMMAND_DELETE | APPLICATION_COMMAND_UPDATE | CHANNEL_CREATE | CHANNEL_DELETE | CHANNEL_PINS_UPDATE | CHANNEL_UPDATE | GUILD_BAN_ADD | GUILD_BAN_REMOVE | GUILD_CREATE | GUILD_DELETE | GUILD_EMOJIS_UPDATE | GUILD_INTEGRATIONS_UPDATE | GUILD_MEMBER_ADD | GUILD_MEMBER_REMOVE | GUILD_MEMBERS_CHUNK | GUILD_MEMBER_UPDATE | GUILD_ROLE_CREATE | GUILD_ROLE_DELETE | GUILD_ROLE_UPDATE | GUILD_STICKERS_UPDATE | GUILD_UPDATE | INTEGRATION_CREATE | INTEGRATION_DELETE | INTEGRATION_UPDATE | INTERACTION_CREATE | INVITE_CREATE | INVITE_DELETE | MESSAGE_CREATE | MESSAGE_DELETE | MESSAGE_DELETE_BULK | MESSAGE_REACTION_ADD | MESSAGE_REACTION_REMOVE | MESSAGE_REACTION_REMOVE_ALL | MESSAGE_REACTION_REMOVE_EMOJI | MESSAGE_UPDATE | PRESENCE_UPDATE | STAGE_INSTANCE_CREATE | STAGE_INSTANCE_DELETE | STAGE_INSTANCE_UPDATE | READY | RESUMED | THREAD_CREATE | THREAD_DELETE | THREAD_LIST_SYNC | THREAD_MEMBERS_UPDATE | THREAD_MEMBER_UPDATE | THREAD_UPDATE | TYPING_START | USER_UPDATE | VOICE_SERVER_UPDATE | VOICE_STATE_UPDATE | WEBHOOKS_UPDATE"
+	export type DiscordEvent = "APPLICATION_COMMAND_CREATE | APPLICATION_COMMAND_DELETE | APPLICATION_COMMAND_UPDATE | CHANNEL_CREATE | CHANNEL_DELETE | CHANNEL_PINS_UPDATE | CHANNEL_UPDATE | GUILD_BAN_ADD | GUILD_BAN_REMOVE | GUILD_CREATE | GUILD_DELETE | GUILD_EMOJIS_UPDATE | GUILD_INTEGRATIONS_UPDATE | GUILD_MEMBER_ADD | GUILD_MEMBER_REMOVE | GUILD_MEMBERS_CHUNK | GUILD_MEMBER_UPDATE | GUILD_ROLE_CREATE | GUILD_ROLE_DELETE | GUILD_ROLE_UPDATE | GUILD_SCHEDULED_EVENT_CREATE | GUILD_SCHEDULED_EVENT_UPDATE | GUILD_SCHEDULED_EVENT_DELETE | GUILD_SCHEDULED_EVENT_USER_ADD | GUILD_SCHEDULED_EVENT_USER_REMOVE | GUILD_STICKERS_UPDATE | GUILD_UPDATE | INTEGRATION_CREATE | INTEGRATION_DELETE | INTEGRATION_UPDATE | INTERACTION_CREATE | INVITE_CREATE | INVITE_DELETE | MESSAGE_CREATE | MESSAGE_DELETE | MESSAGE_DELETE_BULK | MESSAGE_REACTION_ADD | MESSAGE_REACTION_REMOVE | MESSAGE_REACTION_REMOVE_ALL | MESSAGE_REACTION_REMOVE_EMOJI | MESSAGE_UPDATE | PRESENCE_UPDATE | STAGE_INSTANCE_CREATE | STAGE_INSTANCE_DELETE | STAGE_INSTANCE_UPDATE | READY | RESUMED | THREAD_CREATE | THREAD_DELETE | THREAD_LIST_SYNC | THREAD_MEMBERS_UPDATE | THREAD_MEMBER_UPDATE | THREAD_UPDATE | TYPING_START | USER_UPDATE | VOICE_SERVER_UPDATE | VOICE_STATE_UPDATE | WEBHOOKS_UPDATE"
 
 	export interface Interaction {
 		id: string
@@ -55,6 +55,7 @@ declare module "tiny-discord" {
 		url?: string
 		session?: string
 		sequence?: number
+		identifyHook?: (id: number) => { time: number, ask?: boolean } | Promise<{ time: number, ask?: boolean }>
 	}
 	export interface ShardEvent {
 		op: number
@@ -134,29 +135,55 @@ declare module "tiny-discord" {
 		close(): Promise<void>
 		lastPing: number
 		status: number
+		token: string
+		intents: number
+		id: number
+		total: number
+		large_threshold?: number
+		presence?: ShardOptions["presence"]
+		properties: ShardOptions["properties"]
+		version: number
+		encoding: ShardOptions["encoding"]
+		compression: ShardOptions["compression"]
+		url: string
+		session?: string
+		sequence: number
+		identifyHook?: ShardOptions["identifyHook"]
 	}
 
 	export interface InternalSharderOptions {
-		options: Omit<ShardOptions, "session" | "sequence">
+		options: Omit<ShardOptions, "session" | "sequence" | "id" | "total">
 		total: number
 		ids?: number[]
-		shardOptions?: { [id: string]: Omit<ShardOptions, "token" | "intents"> }
+		shardOptions?: Record<string | number, Omit<ShardOptions, "token" | "intents" | "id" | "total">>
 		concurrency?: number
 		identifyHook?: (id) => { time?: number, ask?: boolean }
+	}
+	export interface ControllerObject {
+		total: number
+		remaining: number
+		resetTimestamp: number
+		concurrency: number
+		timeout: number
 	}
 	export class InternalSharder extends EventEmitter {
 		constructor(options: InternalSharderOptions)
 		on(event: DiscordEvent, callback: (data: object, id: number) => void): this
 		on(event: "event", callback: (data: ShardEvent, id: number) => void): this
-		on(event: "debug", callback: (data: string) => void): this
-		on(event: "connect", callback: (id: number) => void): this
+		on(event: "debug", callback: (data: string, id: number) => void): this
 		on(event: "ready", callback: (data: ShardReady, id: number) => void): this
 		on(event: "resumed", callback: (data: ShardResumed, id: number) => void): this
-		on(event: "error", callback: (error: Error, id: number) => void): this
-		connect(): void
+		on(event: "close", callback: (data: Error, id: number) => void): this
+		connect(): Promise<void>
 		close(): Promise<void>
 		getAveragePing(): number
-		getSessions(): { [id: string]: { session: string, sequence: number } }
+		getSessions(): Record<string | number, { session: string, sequence: number }>
+		options: InternalSharderOptions
+		shardOptions: Record<string | number, ShardOptions>
+		ids: number[]
+		total: number
+		shards: Map<number, WebsocketShard>
+		controller: ControllerObject
 	}
 
 	export interface RestClientOptions {
