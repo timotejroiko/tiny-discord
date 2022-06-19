7d51b25c5cd9a9e60c4ece84c6dfd732325fdb8e
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2022-06-19 19:13:05 -0300

    refactor RestClient, move to jsdoc

--- src/RestClient.js
+++ src/RestClient.js
@@ -5,6 +5,15 @@ const { randomBytes } = require("crypto");
 const { Readable } = require("stream");
 
 class RestClient {
+	/**
+	 * @param {{
+	 * 		token: string,
+	 * 		version?: number,
+	 * 		type?: string,
+	 * 		retries?: number,
+	 * 		timeout?: number
+	 * }} options 
+	 */
 	constructor(options) {
 		if(typeof options.token !== "string") { throw new Error("Invalid token"); }
 		this.token = options.token;
@@ -12,8 +21,19 @@ class RestClient {
 		this.type = typeof options.type === "string" && options.type.toLowerCase() === "bearer" ? "Bearer" : "Bot";
 		this.retries = Number(options.retries) || 3;
 		this.timeout = Number(options.timeout) || 10000;
+		/**
+		 * @type {Agent}
+		 * @private
+		 */
 		this._agent = new Agent({ keepAlive: true });
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	get(path, options = {}) {
 		return this.request({
 			...options,
@@ -21,6 +41,13 @@ class RestClient {
 			method: "GET"
 		});
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	delete(path, options = {}) {
 		return this.request({
 			...options,
@@ -28,6 +55,14 @@ class RestClient {
 			method: "DELETE"
 		});
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {{ [key: string]: any, files?: { name: string, data: Buffer | Readable, type?: string }[] } | Buffer} body 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	post(path, body, options = {}) {
 		return this.request({
 			...options,
@@ -36,6 +71,14 @@ class RestClient {
 			method: "POST"
 		});
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {{ [key: string]: any, files?: { name: string, data: Buffer | Readable, type?: string }[] } | Buffer} body 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	patch(path, body, options = {}) {
 		return this.request({
 			...options,
@@ -44,6 +87,14 @@ class RestClient {
 			method: "PATCH"
 		});
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {{ [key: string]: any, files?: { name: string, data: Buffer | Readable, type?: string }[] } | Buffer} body 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	put(path, body, options = {}) {
 		return this.request({
 			...options,
@@ -52,6 +103,13 @@ class RestClient {
 			method: "PUT"
 		});
 	}
+
+	/**
+	 * 
+	 * @param {string} path 
+	 * @param {import("https").RequestOptions} options 
+	 * @returns 
+	 */
 	cdn(path, options = {}) {
 		return this.request({
 			...options,
@@ -60,10 +118,46 @@ class RestClient {
 			cdn: true
 		});
 	}
-	request({ path, method, body, headers = {}, options = {}, retries = this.retries, timeout = this.timeout, _retries = 0, cdn = false }) {
-		let abort;
+
+	/**
+	 * 
+	 * @param {{
+	 * 		path: string,
+	 * 		method: string,
+	 * 		body?: { [key: string]: any, files?: { name: string, data: Buffer | Readable, type?: string }[] } | Buffer,
+	 * 		headers?: import("http").OutgoingHttpHeaders,
+	 * 		options?: import("https").RequestOptions,
+	 * 		retries?: number,
+	 * 		timeout?: number,
+	 * 		cdn?: boolean
+	 * }} options
+	 * @returns {Promise<{ status: number, headers: import("http").IncomingHttpHeaders, body: Record<string, any> | string | Buffer }> & { abort: (reason: string) => void }}
+	 */
+	request({ path, method, body, headers = {}, options = {}, retries = this.retries, timeout = this.timeout, cdn = false}, _retryCount = 0) {
+		let _aborted = false;
+		let _request;
+		let _response;
+		let _resolve;
+		let _reject;
+		let _timer;
+		const abort = reason => {
+			if(_aborted) { return; }
+			_aborted = true;
+			if(_response) { _response.destroy(); }
+			_request.destroy();
+			clearTimeout(_timer);
+			_reject(reason);
+		};
+		const done = data => {
+			if(_aborted) { return; }
+			clearTimeout(_timer);
+			_resolve(data);
+		};
+		_timer = setTimeout(() => abort(new Error("Request timed out")), timeout);
 		const promise = new Promise((resolve, reject) => {
-			const req = request({
+			_resolve = resolve;
+			_reject = reject;
+			_request = request({
 				...options,
 				hostname: cdn ? "cdn.discordapp.com" : "discord.com",
 				port: 443,
@@ -77,46 +171,26 @@ class RestClient {
 					"Authorization": `${this.type} ${this.token}`
 				}
 			});
-			let aborted = false;
-			let response;
-			let timer;
-			abort = reason => {
-				if(aborted) { return; }
-				aborted = true;
-				if(response) { response.destroy(); }
-				req.destroy();
-				clearTimeout(timer);
-				reject(reason);
-			};
-			const done = data => {
-				if(aborted) { return; }
-				clearTimeout(timer);
-				resolve(data);
-			};
-			timer = setTimeout(() => abort(new Error("Request timed out")), timeout);
-			req.once("error", err => {
-				if(aborted) { return; }
-				if(req.reusedSocket && err.code === "ECONNRESET" && _retries < retries) {
-					done(this._request({ path, method, body, retries, timeout, _retries: _retries + 1 }));
+			_request.once("error", (/** @type {Error & { code: string }} */ err) => {
+				if(_request.reusedSocket && err.code === "ECONNRESET" && _retryCount < retries) {
+					if(_aborted) { return; }
+					done(this.request({ path, method, body, retries, timeout }, _retryCount + 1));
 				} else {
 					abort(err);
 				}
 			});
-			req.once("response", res => {
-				response = res;
+			_request.once("response", res => {
+				_response = res;
 				res.once("aborted", () => {
-					if(aborted) { return; }
 					abort(new Error("Received abort event"));
 				});
 				res.on("error", err => {
-					if(aborted) { return; }
 					abort(err);
 				});
 				const data = [];
 				res.on("data", d => data.push(d));
 				res.once("end", () => {
-					if(res.aborted || !res.complete) {
-						if(aborted) { return; }
+					if(!res.complete) {
 						return abort(new Error("Received incomplete message"));
 					}
 					const type = res.headers["content-type"];
@@ -136,56 +210,58 @@ class RestClient {
 						done({
 							status: res.statusCode,
 							headers: res.headers,
-							body: type.startsWith("text") ? body.toString() : body
+							body: type?.startsWith("text") ? body.toString() : body
 						});
 					}
 				});
 			});
 			if(body) {
 				if(Buffer.isBuffer(body)) {
-					req.write(body);
-					req.end();
+					_request.write(body);
+					_request.end();
 				} else if(Array.isArray(body.files)) {
 					const files = body.files;
 					const boundary = randomBytes(16).toString("base64");
 					const writer = async () => {
-						req.setHeader("Content-Type", `multipart/form-data; boundary=${boundary}`);
+						_request.setHeader("Content-Type", `multipart/form-data; boundary=${boundary}`);
 						for(let i = 0; i < files.length; i++) {
 							const file = files[i];
 							if(!file || typeof file !== "object" || !file.name || !file.data) { throw new Error("Invalid file object"); }
 							const type = typeof file.type === "string" ? `\r\nContent-Type: ${file.type}` : "";
-							req.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="files[${i}]"; filename="${file.name}"${type}\r\n\r\n`);
-							if(file.data instanceof Readable) {
+							_request.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="files[${i}]"; filename="${file.name}"${type}\r\n\r\n`);
+							if(file.data instanceof Readable && file.data.readable) {
 								for await(const chunk of file.data) {
-									req.write(chunk);
+									_request.write(chunk);
 								}
 							} else {
-								req.write(file.data);
+								_request.write(file.data);
 							}
 						}
 						if(body.payload_json) {
 							const json = JSON.stringify(body.payload_json);
-							req.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="payload_json"\r\nContent-Type: application/json\r\n\r\n${json}`);
+							_request.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="payload_json"\r\nContent-Type: application/json\r\n\r\n${json}`);
 						} else {
 							for(const entry of Object.entries(body)) {
-								req.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="${entry[0]}"\r\n\r\n${entry[1].toString()}`);
+								_request.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="${entry[0]}"\r\n\r\n${entry[1].toString()}`);
 							}
 						}
-						req.write(`\r\n--${boundary}--`);
-						req.end();
+						_request.write(`\r\n--${boundary}--`);
+						_request.end();
 					};
 					writer().catch(e => abort(e));
 				} else {
 					const data = JSON.stringify(body);
-					req.write(data);
-					req.end();
+					_request.write(data);
+					_request.end();
 				}
 			} else {
-				req.end();
+				_request.end();
 			}
 		});
-		promise.abort = reason => abort(new Error(`Aborted by user: ${reason || "no reason provided"}`));
-		return promise;
+		return {
+			...promise,
+			abort: reason => abort(new Error(`Aborted by user: ${reason || "no reason provided"}`))
+		};
 	}
 }
 
