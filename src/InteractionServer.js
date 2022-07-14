62cf5c895ce9287b4517356970fd6771957e43cb
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2022-07-14 19:06:52 -0300

    InteractionServer: add support for files and rework minor things

--- src/InteractionServer.js
+++ src/InteractionServer.js
@@ -2,9 +2,10 @@
 
 const { EventEmitter } = require("events");
 const { Server } = require("net");
+const { Readable } = require("stream");
 const { createServer } = require("http");
 const { createSecureServer } = require("http2");
-const { createPublicKey, verify } = require("crypto");
+const { createPublicKey, verify, randomBytes } = require("crypto");
 
 class InteractionServer extends EventEmitter {
 	constructor(options) {
@@ -20,20 +21,24 @@ class InteractionServer extends EventEmitter {
 		} catch(e) {
 			throw new Error("Invalid public key - " + e.message);
 		}
-		this.customServer = options.server instanceof Server;
-		this.serverOptions = this.customServer ? null : options.server || {};
 		this.path = typeof options.path === "string" ? options.path : "/";
-		if(this.customServer) {
-			this._server = options.server;
-		} else if(this.serverOptions.cert && this.serverOptions.key) {
-			this._server = createSecureServer(this.serverOptions);
+		if(options.server instanceof Server) {
+			this.serverOptions = null;
+			this.isCustomServer = true;
+			this.server = options.server;
 		} else {
-			this._server = createServer(this.serverOptions);
+			this.isCustomServer = false;
+			this.serverOptions = options.server || {};
+			if("cert" in this.serverOptions && "key" in this.serverOptions) {
+				this.server = createSecureServer(this.serverOptions);
+			} else {
+				this.server = createServer(/** @type {import("http").ServerOptions} */ (this.serverOptions));
+			}
 		}
 		this._attached = false;
 	}
 	listen(port) {
-		const server = this._server;
+		const server = this.server;
 		if(this._attached && server.listening) {
 			return Promise.resolve();
 		}
@@ -43,18 +48,22 @@ class InteractionServer extends EventEmitter {
 			this._attached = true;
 		}
 		if(server.listening) {
-			this.emit("debug", `Attached to port ${server.address().port}`);
+			const address = server.address();
+			const p = address && typeof address === "object" ? address.port : "???";
+			this.emit("debug", `Attached to port ${p}`);
 			return Promise.resolve();
 		}
 		return new Promise(resolve => {
 			server.listen(port || 0, () => {
-				this.emit("debug", `Started listening on port ${server.address().port}`);
+				const address = server.address();
+				const p = address && typeof address === "object" ? address.port : "???";
+				this.emit("debug", `Started listening on port ${p}`);
 				resolve();
 			});
 		});
 	}
 	close() {
-		const server = this._server;
+		const server = this.server;
 		if(!this._attached && !server.listening) {
 			return Promise.resolve();
 		}
@@ -62,7 +71,7 @@ class InteractionServer extends EventEmitter {
 			server.removeListener("request", this._onRequest);
 			server.removeListener("error", this._onError);
 			this._attached = false;
-			if(this.customServer) {
+			if(this.isCustomServer) {
 				this.emit("debug", "Detached from server");
 				return Promise.resolve();
 			}
@@ -81,17 +90,17 @@ class InteractionServer extends EventEmitter {
 		this.emit("error", e);
 	}
 	_onRequest(req, res) {
-		if(!req.url.startsWith(this.path)) { return; }
 		const signature = req.headers["x-signature-ed25519"];
 		const timestamp = req.headers["x-signature-timestamp"];
+		if(!req.url?.startsWith(this.path)) { return; }
 		if(!signature || !timestamp) {
 			this.emit("debug", "Received invalid headers, returning 401");
 			res.writeHead(401);
 			res.end();
 			return;
 		}
-		let body = "";
-		req.on("data", d => body += d.toString());
+		/** @type {Buffer[]} */ const buffer = [];
+		req.on("data", d => buffer.push(d));
 		req.on("end", () => {
 			if(!req.complete) {
 				this.emit("debug", "Received incomplete request, returning 400");
@@ -99,6 +108,7 @@ class InteractionServer extends EventEmitter {
 				res.end();
 				return;
 			}
+			const body = Buffer.concat(buffer).toString();
 			const valid = isValidSignature(this._key, body, timestamp, signature);
 			if(!valid) {
 				this.emit("debug", "Received invalid signature, returning 401");
@@ -143,8 +153,7 @@ class InteractionServer extends EventEmitter {
 				Promise.resolve(response).then(val => {
 					if(isValidResponse(val)) {
 						this.emit("debug", "Responding to interaction");
-						res.writeHead(200, { "Content-Type": "application/json" });
-						res.end(JSON.stringify(val));
+						this._respond(val, res);
 					} else {
 						this.emit("debug", "No valid response provided, returning 500");
 						res.writeHead(500);
@@ -162,9 +171,9 @@ class InteractionServer extends EventEmitter {
 					try {
 						const result = listener(data);
 						const index = responses.length;
-						const promise = Promise.resolve(result).then(val => [index, val]).catch(e => {
+						const promise = Promise.resolve(result).then(val => ({index, val})).catch(e => {
 							this.emit("error", e);
-							return [index];
+							return {index};
 						});
 						responses.push(promise);
 					} catch(e) {
@@ -173,17 +182,14 @@ class InteractionServer extends EventEmitter {
 					}
 				}
 				(async () => {
-					let filtered = [...responses];
-					while(filtered.length) {
-						const [index, val] = await Promise.race(filtered);
+					while(responses.length) {
+						const { index, val } = await Promise.race(responses);
 						if(isValidResponse(val)) {
 							this.emit("debug", "Responding to interaction");
-							res.writeHead(200, { "Content-Type": "application/json" });
-							res.end(JSON.stringify(val));
+							this._respond(val, res);
 							return;
 						}
-						responses[index] = null;
-						filtered = responses.filter(Boolean);
+						responses.splice(index, 1);
 					}
 					this.emit("debug", "No response provided, returning 500");
 					res.writeHead(500);
@@ -191,6 +197,49 @@ class InteractionServer extends EventEmitter {
 				})();
 			}
 		});
+	}
+	_respond(val, res) {
+		if("files" in val && Array.isArray(val.files)) {
+			const boundary = randomBytes(16).toString("base64");
+			const files = val.files;
+			try {
+				const json = JSON.stringify(val.payload_json);
+				res.writeHead(200, { "Content-Type": `multipart/form-data; boundary=${boundary}` });
+				res.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="payload_json"\r\nContent-Type: application/json\r\n\r\n${json}`);
+			} catch(e) {
+				this.emit("debug", "JSON payload produced an error, returning 500");
+				this.emit("error", e);
+				res.writeHead(500);
+				res.end();
+				return;
+			}
+			(async () => {
+				for(let i = 0; i < files.length; i++) {
+					const file = files[i];
+					const type = typeof file.type === "string" ? `\r\nContent-Type: ${file.type}` : "";
+					res.write(`\r\n--${boundary}\r\nContent-Disposition: form-data; name="files[${i}]"; filename="${file.name}"${type}\r\n\r\n`);
+					if(file.data instanceof Readable && file.data.readable) {
+						for await(const chunk of file.data) {
+							res.write(chunk);
+						}
+					} else {
+						res.write(file.data);
+					}
+				}
+				res.end(`\r\n--${boundary}--`);
+			})();
+		} else {
+			try {
+				const json = JSON.stringify(val);
+				res.writeHead(200, { "Content-Type": "application/json" });
+				res.end(json);
+			} catch(e) {
+				this.emit("debug", "Response produced an error, returning 500");
+				this.emit("error", e);
+				res.writeHead(500);
+				res.end();
+			}
+		}
 	}
 }
 
@@ -201,7 +250,16 @@ function isValidSignature(key, body, timestamp, signature) {
 }
 
 function isValidResponse(value) {
-	return value && typeof value === "object" && Number.isInteger(value.type);
+	if(!value || typeof value !== "object") { return false; }
+	if("files" in value) {
+		for(const file of value.files) {
+			if(!file || typeof file !== "object" || !file.name || !file.data) {
+				return false;
+			}
+		}
+		return value.payload_json && typeof value.payload_json === "object" && Number.isInteger(value.payload_json.type);
+	}
+	return Number.isInteger(value.type);
 }
 
 module.exports = InteractionServer;
